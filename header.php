1dce0c9a7f9b2fdc249e3185d9a9f36ecb5c1e0d
Mike Bryant <mikesname@gmail.com>
2019-05-07 15:04:36 +0200

    Make header markup more semantic and less reliant on BS classes
    Also make transforms more mobile-first.

--- header.php
+++ header.php
@@ -39,17 +39,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<a class="skip-link sr-only sr-only-focusable"
 			   href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-			<nav class="navbar navbar-expand-lg navbar-dark bg-secondary" id="primary-nav">
-				<div class="container">
+			<nav id="primary-nav">
+				<div id="primary-nav-container">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 						<img id="header-logo" class="img-fluid"
 						     src="<?php echo get_theme_file_uri( "img/ehri-logo@2x.png" ); ?>"
 						     alt=""/></a>
 
-					<h1 class="navbar-brand mb-0 nav-item"><a class="nav-link" rel="home"
-					                                          href="<?php echo esc_url( home_url( '/' ) ); ?>"
-					                                          title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-					                                          itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+					<h1 id="site-title">
+						<a rel="home"
+						  href="<?php echo esc_url( home_url( '/' ) ); ?>"
+						  title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+						  itemprop="url"><?php bloginfo( 'name' ); ?>
+						</a>
+					</h1>
 
 					<button class="navbar-toggler" type="button" data-toggle="collapse"
 					        data-target="#navbar-nav-dropdown"
@@ -60,10 +63,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 					<!-- The WordPress Menu goes here -->
 					<div class="collapse navbar-collapse" id="navbar-nav-dropdown">
-						<ul id="navbar-nav-pages" class="navbar-nav ml-auto">
+						<ul id="navbar-nav-pages">
 							<?php $active_articles = is_archive() || is_single(); ?>
-							<li class="nav-item dropdown <?php if ($active_articles) echo "active"; ?>">
-								<a class="nav-link dropdown-toggle" id="navbar-cat-dropdown" data-toggle="dropdown"
+							<li class="dropdown <?php if ($active_articles) echo "active"; ?>">
+								<a class="dropdown-toggle" id="navbar-cat-dropdown" data-toggle="dropdown"
 								   href="#">Articles</a>
 								<div class="dropdown-menu" id="categories-dropdown"
 								     aria-labelledby="navbar-cat-dropdown">
@@ -78,8 +81,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 							</li>
 
 							<?php foreach ( wp_get_nav_menu_items( 'main' ) as $item ): ?>
-								<li class="nav-item <?php if ( $item->object_id == get_the_ID() ) echo "active"; ?>">
-									<a class="nav-link" href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+								<li class="<?php if ( $item->object_id == get_the_ID() ) echo "active"; ?>">
+									<a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
 								</li>
 							<?php endforeach; ?>
 						</ul>
